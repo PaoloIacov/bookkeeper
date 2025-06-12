a75025e68a8238ae2b38a4b6673ca4f7b394dee5
PaoloIacov <iacovpaolo1@gmail.com>
2025-06-12 17:43:04 +0200

    First test of Github Action

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/mytests/DummyTest.java
@@ -0,0 +1,13 @@
+package org.apache.bookkeeper.mytests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTestShouldPass() {
+        assertTrue(true);
+    }
+}
---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/mytests/MyProjectTestSuite.java
@@ -0,0 +1,12 @@
+package org.apache.bookkeeper.mytests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        DummyTest.class
+        // aggiungi qui gli altri test
+})
+public class MyProjectTestSuite {
+}
